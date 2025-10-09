1c920d63f15913dbdeea77ce83a13b824f271369
berni <bernizapico@gmail.com>
2025-10-09 16:12:27 -0300

    feat: now you can pass arguments to damped oscillator

--- src/main/java/DampedOscillatorMain.java
+++ src/main/java/DampedOscillatorMain.java
@@ -7,24 +7,35 @@ import tools.PostProcessor;
 import java.io.IOException;
 import java.util.Iterator;
 public class DampedOscillatorMain {
+    private static final String DT = "DT";
+    private static final String ESTIMATOR = "estimator";
     public static void main(String[] args) throws IOException {
-        double deltaT = Math.pow(10, -2);
-        MovementModel model = new DampedOscillator(Math.pow(10, 4), 100, 1, 70);
-        EstimationMethod estimationMethod = new EstimationMethod(model, deltaT, 5);
+        String estimator = System.getProperty(ESTIMATOR);
+        double deltaT = Double.parseDouble(System.getProperty(DT));
+        double A = 1;
+        double gamma = 100;
+        double mass = 70;
+        double endTime = 5;
+        MovementModel model = new DampedOscillator(Math.pow(10, 4), gamma, A, mass);
+        EstimationMethod estimationMethod = new EstimationMethod(model, deltaT, endTime);
         Iterator<Time> timeIt;
-        timeIt = estimationMethod.verletEstimation();
-        try (PostProcessor postProcessor = new PostProcessor("verlet.txt")) {
-            timeIt.forEachRemaining(postProcessor::processTime);
+        if (estimator.equals("verlet")) {
+            timeIt = estimationMethod.verletEstimation();
+            try (PostProcessor postProcessor = new PostProcessor("verlet.txt")) {
+                timeIt.forEachRemaining(postProcessor::processTime);
+            }
         }
-
-        timeIt = estimationMethod.beemanEstimation();
-        try (PostProcessor postProcessor = new PostProcessor("beeman.txt")) {
-            timeIt.forEachRemaining(postProcessor::processTime);
+        else if (estimator.equals("beeman")) {
+            timeIt = estimationMethod.beemanEstimation();
+            try (PostProcessor postProcessor = new PostProcessor("beeman.txt")) {
+                timeIt.forEachRemaining(postProcessor::processTime);
+            }
         }
-
-        timeIt = estimationMethod.gearEstimation();
-        try (PostProcessor postProcessor = new PostProcessor("gear.txt")) {
-            timeIt.forEachRemaining(postProcessor::processTime);
+        else {
+            timeIt = estimationMethod.gearEstimation();
+            try (PostProcessor postProcessor = new PostProcessor("gear.txt")) {
+                timeIt.forEachRemaining(postProcessor::processTime);
+            }
         }
     }
 }
